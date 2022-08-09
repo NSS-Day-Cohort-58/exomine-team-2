d4b576c6f4c234d13c0bd77fcc82dfd086b64bfa
Carly Doppelheuer <107224963+carlydopps@users.noreply.github.com>
2022-08-09 13:11:26 -0600

    Update facilities.js

--- scripts/facilities.js
+++ scripts/facilities.js
@@ -5,19 +5,6 @@ const facilities = getFacilities()
 
 
 
-document.addEventListener(
-    "change",
-    (event) => {
-        if (event.target.name === "facility") {
-            setFacility((parseInt(event.target.value)))
-        }
-    }
-)
-
-
-
-
-
 export const Facilities = () => {
     let html = `<select id="facility">
     <option value="0">Select Facility...</option>`
